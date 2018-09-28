19f9dd9cda6ec6c4ef7643d3c912d57e86673fdb
kedi <904228465@qq.com>
2018-09-28 20:30:01 +1000

    add DressShoe.java

--- src/lab8/Shoe.java
+++ src/lab8/Shoe.java
@@ -2,36 +2,41 @@ package lab8;
 
 public class Shoe {
 	private String color;
-    private String designer;
-    private double size;
-    
-    public Shoe() {
-    	 color = "";
-         designer = "";
-         size = 0;
+	private String designer;
+	private double size;
+
+	public Shoe() {
+		color = "";
+		designer = "";
+		size = 0;
 	}
-    
-    public Shoe(String c, String d, double s){
-    	setColor(c);
-    	setDesigner(d);
-    	setSize(s);
-    }
-    
+
+	public Shoe(String c, String d, double s) {
+		setColor(c);
+		setDesigner(d);
+		setSize(s);
+	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
+
 	public String getDesigner() {
 		return designer;
 	}
+
 	public void setDesigner(String designer) {
 		this.designer = designer;
 	}
+
 	public double getSize() {
 		return size;
 	}
+
 	public void setSize(double size) {
 		if (size > 0) {
 			this.size = size;
